f29d40a20102158ae089bc14ef91bd479a8f9282
tonmanayo <tonmanayo@hotmail.com>
2017-06-11 20:32:46 +0200

    same struct

--- libs/lib1-opengl/inc/OpenGL.hpp
+++ libs/lib1-opengl/inc/OpenGL.hpp
@@ -5,4 +5,31 @@
 #ifndef NIBBLER_OPENGL_HPP
 #define NIBBLER_OPENGL_HPP
 
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_primitives.h"
+
+#include <vector>
+#include "../../inc/ILibrary.hpp"
+
+class OpenGL : public ILibrary {
+public:
+	OpenGL(int winWidth, int winHeight);
+	OpenGL(OpenGL const &openGL);
+	OpenGL operator=(OpenGL const &rhs );
+
+	int keyhook();
+	void print(std::vector<SnakePart*> snakeParts);
+
+private:
+	OpenGL();
+	const unsigned int  _winWidth;
+	const unsigned int  _winHeight;
+	const unsigned int _blockSize;
+	const unsigned int _FPS;
+	ALLEGRO_DISPLAY     *_display;
+	ALLEGRO_EVENT_QUEUE *_event_queue;
+	ALLEGRO_TIMER       *_timer;
+	ALLEGRO_BITMAP      *_box;
+};
 #endif //NIBBLER_OPENGL_HPP
--- libs/lib1-opengl/src/OpenGL.cpp
+++ libs/lib1-opengl/src/OpenGL.cpp
@@ -2,3 +2,95 @@
 // Created by Kyle GANI on 2017/06/08.
 //
 
+int keyhook(){
+	unsigned int direction = 0;
+	if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+		switch(ev.keyboard.keycode) {
+			case ALLEGRO_KEY_UP:
+				direction = 1;
+			case ALLEGRO_KEY_RIGHT:
+				direction = 2;
+			case ALLEGRO_KEY_DOWN:
+				direction = 3;
+			case ALLEGRO_KEY_LEFT:
+				direction = 4;
+		}
+		_redraw = true;
+	}
+	return direction;
+}
+
+void print(std::vector<SnakePart*> snakeParts){
+	if(redraw && al_is_event_queue_empty(event_queue)) {
+			al_clear_to_color(al_map_rgb(0,0,0));
+		for (auto i = snakeParts.begin(); i < snakeParts.end(); i++){
+			al_draw_bitmap(block, (*i)->getPosX() - 20/2, (*i)->getPosY() + 20/2, 0);
+		}
+		al_flip_display();
+		_redraw = false;
+	}
+}
+
+OpenGL::OpenGL(int width, int height) : _winWidth(width), _winHeight(height)
+{
+	_block_Size = 32;
+	_FPS = 60;
+	_display = NULL;
+	_event_queue = NULL;
+	_box = NULL;
+
+	if(!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return ;
+	}
+
+	if(!al_install_keyboard()) {
+		fprintf(stderr, "failed to initialize the keyboard!\n");
+		return ;
+	}
+
+	_timer = al_create_timer(1.0 / _FPS);
+	if(!_timer) {
+		fprintf(stderr, "failed to create timer!\n");
+		return -1;
+	}
+
+	_display = al_create_display(_winWidth, _winHeight);
+	if(!_display) {
+		fprintf(stderr, "failed to create display!\n");
+		al_destroy_timer(_timer);
+		return ;
+	}
+
+	_box = al_create_bitmap(_blockSize, _blockSize);
+	if(!_box) {
+		fprintf(stderr, "failed to create block bitmap!\n");
+		al_destroy_display(_display);
+		al_destroy_timer(_timer);
+		return ;
+	}
+
+	al_set_target_bitmap(_box);
+	al_clear_to_color(al_map_rgb(255, 0, 255));
+	al_set_target_bitmap(al_get_backbuffer(_display));
+
+	_event_queue = al_create_event_queue();
+	if(!_event_queue) {
+		fprintf(stderr, "failed to create event_queue!\n");
+		al_destroy_bitmap(_box);
+		al_destroy_display(_display);
+		al_destroy_timer(_timer);
+		return ;
+	}
+
+	al_register_event_source(_event_queue, al_get_display_event_source(_display));
+	al_register_event_source(_event_queue, al_get_timer_event_source(_timer));
+	al_register_event_source(_event_queue, al_get_keyboard_event_source());
+	al_clear_to_color(al_map_rgb(0,0,0));
+	al_flip_display();
+	al_start_timer(_timer);
+}
+
+ILibrary *create(int winWidth, int winHeight){
+	return new OpenGL(winWidth, winHeight);
+}
