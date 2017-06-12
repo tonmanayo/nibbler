cfd6b967e5cfbbc2bd27a6b8adbaf76a843ee548
Kyle Gani <kyle@everest.digital>
2017-06-12 14:52:41 +0200

    fixed install.sh sdl typo

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 echo Installing dependencies
-echo Installing SDL
 if brew ls --versions sdl2  > /dev/null; then
   echo SDL2 already installed
 else
   echo Installing SDL2
-  brew install sld2
+  brew install sdl2
 fi
