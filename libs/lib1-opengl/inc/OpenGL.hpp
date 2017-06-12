d7bf9e28d440d318c782ad29a6475c6797c031f3
Tony MACK <tmack@e5r6p10.jnb.42.fr>
2017-06-12 18:01:31 +0200

    please check

--- libs/lib1-opengl/inc/OpenGL.hpp
+++ libs/lib1-opengl/inc/OpenGL.hpp
@@ -6,9 +6,8 @@
 #define NIBBLER_OPENGL_HPP
 
 #include "allegro5/allegro.h"
-#include "allegro5/allegro_image.h"
-#include "allegro5/allegro_primitives.h"
-
+#include <cstdlib>
+#include <iostream>
 #include <vector>
 #include "../../inc/ILibrary.hpp"
 
@@ -17,19 +16,32 @@ public:
 	OpenGL(int winWidth, int winHeight);
 	OpenGL(OpenGL const &openGL);
 	OpenGL operator=(OpenGL const &rhs );
+	virtual ~OpenGL();
 
 	int keyhook();
 	void print(std::vector<SnakePart*> snakeParts);
 
 private:
 	OpenGL();
-	const unsigned int  _winWidth;
-	const unsigned int  _winHeight;
+	const unsigned int _winWidth;
+	const unsigned int _winHeight;
 	const unsigned int _blockSize;
 	const unsigned int _FPS;
+    bool               _redraw;
 	ALLEGRO_DISPLAY     *_display;
 	ALLEGRO_EVENT_QUEUE *_event_queue;
 	ALLEGRO_TIMER       *_timer;
 	ALLEGRO_BITMAP      *_box;
 };
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern "C" ILibrary* create(int winWidth, int WinHeight);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif //NIBBLER_OPENGL_HPP
--- libs/lib1-opengl/src/OpenGL.cpp
+++ libs/lib1-opengl/src/OpenGL.cpp
@@ -1,44 +1,12 @@
 //
 // Created by Kyle GANI on 2017/06/08.
 //
+#include "../inc/OpenGL.hpp"
 
-int OpenGL::keyhook(){
-	unsigned int direction = 0;
-	if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-		switch(ev.keyboard.keycode) {
-			case ALLEGRO_KEY_UP:
-				direction = 1;
-			case ALLEGRO_KEY_RIGHT:
-				direction = 2;
-			case ALLEGRO_KEY_DOWN:
-				direction = 3;
-			case ALLEGRO_KEY_LEFT:
-				direction = 4;
-		}
-		_redraw = true;
-	}
-	return direction;
-}
 
-void OpenGL::print(std::vector<SnakePart*> snakeParts){
-	if(redraw && al_is_event_queue_empty(event_queue)) {
-			al_clear_to_color(al_map_rgb(0,0,0));
-		for (auto i = snakeParts.begin(); i < snakeParts.end(); i++){
-			al_draw_bitmap(block, (*i)->getPosX() - 20/2, (*i)->getPosY() + 20/2, 0);
-		}
-		al_flip_display();
-		_redraw = false;
-	}
-}
-
-OpenGL::OpenGL(int width, int height) : _winWidth(width), _winHeight(height)
-{
-	_block_Size = 32;
-	_FPS = 60;
-	_display = NULL;
-	_event_queue = NULL;
-	_box = NULL;
-
+OpenGL::OpenGL(int width, int height) : _winWidth(width), _winHeight(height), _blockSize(20), _FPS(60) {
+    std::cout << "initialized OpenGL library at Width: " << _winWidth << " and Height: " << _winHeight << std::endl;
+	_redraw = true;
 	if(!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
 		return ;
@@ -52,7 +20,7 @@ OpenGL::OpenGL(int width, int height) : _winWidth(width), _winHeight(height)
 	_timer = al_create_timer(1.0 / _FPS);
 	if(!_timer) {
 		fprintf(stderr, "failed to create timer!\n");
-		return -1;
+		return ;
 	}
 
 	_display = al_create_display(_winWidth, _winHeight);
@@ -64,7 +32,7 @@ OpenGL::OpenGL(int width, int height) : _winWidth(width), _winHeight(height)
 
 	_box = al_create_bitmap(_blockSize, _blockSize);
 	if(!_box) {
-		fprintf(stderr, "failed to create block bitmap!\n");
+		fprintf(stderr, "failed to create box bitmap!\n");
 		al_destroy_display(_display);
 		al_destroy_timer(_timer);
 		return ;
@@ -91,8 +59,54 @@ OpenGL::OpenGL(int width, int height) : _winWidth(width), _winHeight(height)
 	al_start_timer(_timer);
 }
 
+OpenGL::~OpenGL(){
+	al_destroy_bitmap(_box);
+	al_destroy_timer(_timer);
+	al_destroy_display(_display);
+	al_destroy_event_queue(_event_queue);
+}
+
+int OpenGL::keyhook(){
+	ALLEGRO_EVENT ev;
+	int direction = 0;
+	al_wait_for_event(_event_queue, &ev);
+	if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+		switch(ev.keyboard.keycode) {
+			case ALLEGRO_KEY_UP:
+				direction = 1;
+				break ;
+			case ALLEGRO_KEY_RIGHT:
+				direction = 2;
+				break ;
+			case ALLEGRO_KEY_DOWN:
+				direction = 3;
+				break ;
+			case ALLEGRO_KEY_LEFT:
+				direction = 4;
+				break ;
+			case ALLEGRO_KEY_ESCAPE:
+				direction = -1;
+				break;
+		}
+		_redraw = true;
+	}
+	return direction;
+}
+
+void OpenGL::print(std::vector<SnakePart*> snakeParts){
+	if(_redraw && al_is_event_queue_empty(_event_queue)) {
+		_redraw = false;
+		al_clear_to_color(al_map_rgb(0,0,0));
+		for (auto i = snakeParts.begin(); i < snakeParts.end(); i++){
+			al_draw_bitmap(_box, (*i)->getPosX() - 20/2, (*i)->getPosY() + 20/2, 0);
+		}
+		al_flip_display();
+	}
+}
+
 ILibrary *create(int winWidth, int winHeight){
 	return new OpenGL(winWidth, winHeight);
 }
 
 //g++ -Wall main.cpp -I/usr/include/allegro5 -L/usr/lib -lallegro -lallegro_image -lallegro_primitives
+//g++ -Wall main.cpp -I/nfs/zfs-student-6/users/tmack/.brew/Cellar/allegro/5.2.2/include -L/nfs/zfs-student-6/users/tmack/.brew/Cellar/allegro/5.2.2/lib -lallegro -lallegro_image -lallegro_primitives
