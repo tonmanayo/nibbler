14096db38eb78b93f02edcb557ca5ab878d8bf94
tonmanayo <tonmanayo@hotmail.com>
2017-06-13 15:25:43 +0200

    SMFL almost done

--- libs/lib3-sfml/inc/SFML.hpp
+++ libs/lib3-sfml/inc/SFML.hpp
@@ -1,8 +1,43 @@
 //
 // Created by Kyle GANI on 2017/06/08.
-//
+//sudo apt-get install libsfml-dev
 
 #ifndef NIBBLER_SFML_HPP
 #define NIBBLER_SFML_HPP
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include "../../inc/ILibrary.hpp"
+
+class SFML : public ILibrary {
+public:
+	SFML(int winWidth, int winHeight);
+	SFML(SFML const &rhs);
+	SFML operator=(SFML const &rhs );
+	virtual ~SFML();
+
+	int keyhook();
+	void print(std::vector<SnakePart*> snakeParts);
+
+private:
+	SFML();
+	const unsigned int _winWidth;
+	const unsigned int _winHeight;
+	const unsigned int _blockSize;
+	sf::RenderWindow _window;
+};
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ILibrary* create(int winWidth, int winHeight);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //NIBBLER_SFML_HPP
--- libs/lib3-sfml/src/SFML.cpp
+++ libs/lib3-sfml/src/SFML.cpp
@@ -2,3 +2,50 @@
 // Created by Kyle GANI on 2017/06/08.
 //
 
+#include "../inc/SFML.hpp"
+
+SFML::SFML(int winWidth, int winHeight) : _winWidth(winWidth), _winHeight(winHeight), _window(sf::VideoMode(200, 200), "Nibbler!") ,_blockSize(20)
+{
+	std::cout << "initialized SFML library at Width: " << _winWidth << " and Height: " << _winHeight << std::endl;
+}
+
+SFML::~SFML(){
+	std::cout << "Dead SFML\n";
+}
+
+int SFML::keyhook(){
+
+	//int direction = 0;
+	sf::Event ev;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		return 4;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		return 2;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		return 1;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		return 3;
+	} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+		return -1;
+	}
+	if (ev.type == sf::Event::Closed){
+		_window.close();
+		return -1;
+	}
+	return 0;
+}
+
+void SFML::print(std::vector<SnakePart*> snakeParts){
+	sf::RectangleShape _shape(sf::Vector2f(_blockSize, _blockSize));
+	_shape.setPosition(_winHeight / 2,_winWidth / 2);
+	_shape.setFillColor(sf::Color::Green);
+	for (auto i = snakeParts.begin(); i < snakeParts.end(); i++){
+		_shape.setPosition((*i)->getPosX() - 20/2,(*i)->getPosY() + - 20/2);
+		_window.clear();
+		_window.draw(_shape);
+	}
+	_window.display();
+}
